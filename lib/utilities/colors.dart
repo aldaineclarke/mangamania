e884c4700261befc8da9806c49200cc89fe3f34d
aldaine-AG <aldaineclarke1@gmail.com>
2022-11-02 10:22:39 -0500

    created utilities folder for colors

---
+++ lib/utilities/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class AppColors{
+  static final primaryColor =  Color(0xFF181F36);
+  static final secondaryColor = Color.fromARGB(255, 109, 143, 255);
+  static final textColor = Color.fromARGB(255, 63, 84, 155);
+  static final tertiaryColor = Color.fromARGB(255, 109, 143, 255);
+  static final titleColor = Color(0xFFBACAFF);
+}
