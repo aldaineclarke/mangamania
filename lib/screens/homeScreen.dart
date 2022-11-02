1d6cc105eb2fcd3ccf22f04d1e57c2d2966a5e90
aldaine-AG <aldaineclarke1@gmail.com>
2022-11-02 11:30:08 -0500

    updated navigation style

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -15,7 +15,16 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
         backgroundColor: AppColors.primaryColor,
         appBar: AppBar(
-
+          iconTheme: IconThemeData(color : AppColors.titleColor, size: 30),
+          backgroundColor: AppColors.primaryColor,
+          centerTitle: true,
+          title: Image(image:AssetImage("assets/moviemania_logo.png"), height: 30.0,),
+          actions: [
+            TextButton(
+              onPressed: (){},
+              child: Icon(Icons.search_rounded, size: 30.0, color:AppColors.titleColor)
+            )
+          ],
         ),
         body: Center(child:Container()),
         drawer: SafeArea(
